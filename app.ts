a1051aae2b74dd58be207c76d4df0fc410dafb33
usman-pasha-786 <147035343+usman-pasha-786@users.noreply.github.com>
2024-01-02 05:25:14 -0800

    uploaded files

---
+++ app.ts
@@ -0,0 +1,52 @@
+import inquirer from "inquirer";
+const user = await inquirer.prompt([{
+    type:"input",
+    name : "userName",
+    message:" please enter your name,",
+    validate: (ans)=>{
+        if(!ans){
+            return "Kindly enter your name"
+        }return true
+    }
+},{
+    type:"number",
+    name : "userAge",
+    message:"What is your age ?",
+    
+},{
+    type:"list",
+    name : "userGender",
+    message:"Are you male or female ?",
+    choices: ["male","female"]
+    
+},{
+    type:"list",
+    name : "userMaritalStatus",
+    message:"Are you married or unmarried ?",
+    choices: ["Married","Unmarried"]
+    
+},{
+    type:"checkbox",
+    name : "userhobby",
+    message:"What is your favourite games ?",
+    choices: ["cricket","football","hocky","boxing","snooker"],
+    default:"nothing"
+},{
+    type:"number",
+    name : "userNumber",
+    message:"Enter your mobile number ?",
+    default:"nothing"
+},{
+    type:"input",
+    name : "userEmail",
+    message:"Kindly type your email ?",
+    default:"nothing"
+    
+},{
+    type:"list",
+    name : "userLocation",
+    message:"kindly select your country ?",
+    choices:["pakistan","usa","canada","iran"]
+    
+}])
+console.log(user)
